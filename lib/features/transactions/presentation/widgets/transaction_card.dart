9eaad9749756b2fe710737cbbf324f1196b712c1
levmorgot <37115538+levmorgot@users.noreply.github.com>
2024-05-14 19:05:08 +0300

    date info

--- lib/features/transactions/presentation/widgets/transaction_card.dart
+++ lib/features/transactions/presentation/widgets/transaction_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:intl/intl.dart';
 import 'package:otlozhka/features/transactions/domain/entities/transaction_category_entity.dart';
 import 'package:otlozhka/features/transactions/domain/entities/transaction_entity.dart';
 import 'package:otlozhka/features/transactions/presentation/widgets/category_icon.dart';
@@ -13,18 +14,26 @@ class TransactionCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DecoratedBox(
-        decoration: BoxDecoration(
-          color: Colors.black12,
-          borderRadius: BorderRadius.circular(8.0),
-        ) ,
+      decoration: BoxDecoration(
+        color: Colors.black12,
+        borderRadius: BorderRadius.circular(8.0),
+      ),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Row(
           children: [
             CategoryIcon(category: category, radius: 20),
             const Gap(10),
-            Text(
-              category.name,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  category.name,
+                ),
+                Text(
+                  DateFormat(DateTime.now().year > transaction.transactionDate.year ? 'dd.MM.yy' : 'dd.MM').format(transaction.transactionDate),
+                ),
+              ],
             ),
             const Spacer(),
             Text('${transaction.type == TransactionType.income ? '+' : '-'}${transaction.amount} ₽')
